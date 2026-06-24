2f4ca2b1c641a2d12285a43b1ed670380034fd6e
PAULO HENRIQUE DOS SANTOS <paulo.hsantos26@senacsp.edu.br>
2026-06-23 22:32:59 -0300

    update dependencias package.json

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,12 +1,26 @@
 import { Stack } from 'expo-router'
 import { SQLiteProvider } from 'expo-sqlite'
 import { Suspense } from 'react';
+import { Inter_400Regular, Inter_500Medium, Inter_700Bold, Inter_900Black_Italic, useFonts } from '@expo-google-fonts/inter'
+
 
 import { colors } from '@/theme/colors';
 import { migrate } from '@/database/migrate';
 import { Loading } from '@/components/Loading';
 
 export default function Layout() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Inter_700Bold,
+    Inter_900Black_Italic
+  })
+
+  if (!fontsLoaded) {
+    return <Loading />
+  }
+
+
 
   return (
     <Suspense fallback={<Loading />}>
--- src/components/HomeHeader/index.tsx
+++ src/components/HomeHeader/index.tsx
@@ -6,52 +6,53 @@ import { styles } from './style';
 import { colors } from "@/theme/colors";
 import { Separador } from "@/components/Separador";
 import { Resumo, ResumoProps } from "@/components/Resumo";
+import { Inter_900Black, Inter_900Black_Italic } from "@expo-google-fonts/inter";
 
 export type HomeHeaderProps = {
-    total: string,
-    input: ResumoProps,
-    output: ResumoProps
+  total: string,
+  input: ResumoProps,
+  output: ResumoProps
 }
 
 type Props = {
-    data: HomeHeaderProps
+  data: HomeHeaderProps
 }
 
 export function HomeHeader({ data }: Props) {
-    return (
-        <LinearGradient
-            colors={[colors.blue[500], colors.blue[800]]}
-            style={styles.container}
-        >
+  return (
+    <LinearGradient
+      colors={[colors.blue[500], colors.blue[800]]}
+      style={styles.container}
+    >
 
-            <View>
-                <Text style={styles.label}>Total que você possui</Text>
-                <Text style={styles.total}>{data.total}</Text>
-            </View>
+      <View>
+        <Text style={styles.label}>Total que você possui</Text>
+        <Text style={styles.total}>{data.total}</Text>
+      </View>
 
-            <Separador color={colors.blue[400]} />
-            <View style={styles.resumoContainer}>
-                <Resumo
-                    data={data.input}
-                    icon={{
-                        nomeIcone: "arrow-upward",
-                        corIcone: colors.green[500]
-                    }}
-                />
+      <Separador color={colors.blue[400]} />
+      <View style={styles.resumoContainer}>
+        <Resumo
+          data={data.input}
+          icon={{
+            nomeIcone: "arrow-upward",
+            corIcone: colors.green[500]
+          }}
+        />
 
-                <Resumo
-                    data={data.output}
-                    icon={{
-                        nomeIcone: "arrow-downward",
-                        corIcone: colors.red[500]
-                    }}
-                    isRight={true}
-                />
+        <Resumo
+          data={data.output}
+          icon={{
+            nomeIcone: "arrow-downward",
+            corIcone: colors.red[500]
+          }}
+          isRight={true}
+        />
 
-            </View>
+      </View>
 
-        </LinearGradient>
+    </LinearGradient>
 
 
-    )
+  )
 }
